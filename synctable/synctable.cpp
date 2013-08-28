b8edcedbda38ef94296054dbdacf5bbebb5fcf45
Ryan Haining <rhainin1@binghamton.edu>
2013-08-28 14:26:29 -0400

    Wraps entire synctable.cpp with namespace synclock
    For some reason I qualified everything in synctable.cpp with synclock::
    instead of just wrapping it in namespace synclock{ }.  Fixed it.

--- synctable/synctable.cpp
+++ synctable/synctable.cpp
@@ -1,46 +1,48 @@
 #include "synctable.h"
 
-// SyncTable
+namespace synclock {
 
-synclock::SyncTable::~SyncTable()
-{
-    for (auto iter = this->locks_table.begin(),
-            end = this->locks_table.end();
-            iter != end;
-            ++iter){
-        delete iter->second; 
-    }
-}
+    // SyncTable
 
-
-std::mutex * synclock::SyncTable::get_lock_address(void *addr)
-{
-    std::lock_guard<std::mutex> lock(this->table_lock); // lock the table
-
-    auto itr = this->locks_table.find(addr);
-    if (itr == this->locks_table.end()) {
-        // this is a new entry, in the table and a mutex must be created
-        itr = this->locks_table.insert(
-                std::make_pair(addr, new std::mutex)).first;
+    SyncTable::~SyncTable()
+    {
+        for (auto iter = this->locks_table.begin(),
+                end = this->locks_table.end();
+                iter != end;
+                ++iter){
+            delete iter->second; 
+        }
     }
 
-    std::mutex *var_lock = itr->second; // get the variable's lock
 
-    return var_lock;
-}
+    std::mutex * SyncTable::get_lock_address(void *addr)
+    {
+        std::lock_guard<std::mutex> lock(this->table_lock); // lock the table
+
+        auto itr = this->locks_table.find(addr);
+        if (itr == this->locks_table.end()) {
+            // this is a new entry, in the table and a mutex must be created
+            itr = this->locks_table.insert(
+                    std::make_pair(addr, new std::mutex)).first;
+        }
+
+        std::mutex *var_lock = itr->second; // get the variable's lock
+
+        return var_lock;
+    }
 
 
-// _Table_Locker
+    // _Table_Locker
 
-synclock::_Table_Locker::_Table_Locker(SyncTable & sync, void * addr)
-    : var_lock_holder(*sync.get_lock_address(addr)),
-      finished(false)
-{ }
-
-
-synclock::_Table_Locker::~_Table_Locker() { }
+    _Table_Locker::_Table_Locker(SyncTable & sync, void * addr)
+        : var_lock_holder(*sync.get_lock_address(addr)),
+        finished(false)
+    { }
 
 
-namespace synclock{
+    _Table_Locker::~_Table_Locker() { }
+
+
+    // The Global SyncTable for synchronized() blocks
     SyncTable globalsynctable;
 }
