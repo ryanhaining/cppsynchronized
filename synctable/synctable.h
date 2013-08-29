ae2c47f99c1d25e31a90dc91a0785865115feec0
Ryan Haining <rhainin1@binghamton.edu>
2013-08-29 01:22:58 -0700

    Removes the need for the _Table_Locker class
    Replaces it with an anonymous struct type in the regular synchronized
    blocks.  This seems desirable to me since it prevents creating an extra
    class that is exposed but really shouldn't be.

--- synctable/synctable.h
+++ synctable/synctable.h
@@ -6,11 +6,9 @@
 
 namespace synclock{
     class SyncTable{
-        friend class _Table_Locker;
         private:
             std::unordered_map<void *, std::mutex *> locks_table;
             std::mutex table_lock; 
-            std::mutex * get_lock_address(void *addr);
 
         public:
             SyncTable(){}
@@ -21,6 +19,7 @@ namespace synclock{
             SyncTable & operator=(const SyncTable &) = delete;
             ~SyncTable();
  
+            std::mutex * get_lock_address(void *addr);
     };
     
     // This class is only for use by the synchronized/tablesynchronized blocks
@@ -68,8 +67,12 @@ for(synclock::_Table_Locker _table_locker_obj_ABCDEFAOEUI(TABLE, (void*)(ADDR));
 // It is also exception safe since destructon occurs when an exception
 // causes the block to exit
 #define synchronized(ADDR)  \
-for(synclock::_Table_Locker _table_locker_obj_ABCDEFAOEUI(synclock::globalsynctable, static_cast<void *>(ADDR)); \
-        !_table_locker_obj_ABCDEFAOEUI.finished; \
-        _table_locker_obj_ABCDEFAOEUI.finished = true)
+for(struct {const std::lock_guard<std::mutex> & lg; bool finished;} pair = \
+        {std::lock_guard<std::mutex>( \
+                *synclock::globalsynctable.get_lock_address( \
+                    static_cast<void *>(ADDR))), \
+            false}; \
+        !pair.finished; \
+        pair.finished = true)
 
 #endif // __SYNCHRONIZER__H__
